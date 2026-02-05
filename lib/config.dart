89e23be3a5ccf3720158da95f4478fd19e4b42f6
gajendra82 <gajendra.kumar@globalspace.in>
2026-02-05 11:49:50 +0530

    push

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
-//const String API_BASE_URL = "https://zydus.mediola.in/development/api/";
+const String API_BASE_URL = "https://zydus.mediola.in/development/api/";
 
-const String API_BASE_URL = "https://zydus.mediola.in/pod_dev/api/";
+//const String API_BASE_URL = "https://zydus.mediola.in/pod_dev/api/";
 // const String API_BASE_URL = "https://a83a5178e1bf.ngrok-free.app/api/";
 const String API_DOC_UPLOAD_URL = "${API_BASE_URL}grn/upload-pdf";
 const String API_POD_UPLOAD_URL = "${API_BASE_URL}pod/upload-pdf";
