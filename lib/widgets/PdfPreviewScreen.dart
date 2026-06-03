9d2232335e7a72177c955d081f0aafc41b9da274
gajendra82 <gajendra.kumar@globalspace.in>
2026-06-03 11:24:16 +0530

    web build created

--- lib/widgets/PdfPreviewScreen.dart
+++ lib/widgets/PdfPreviewScreen.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class PdfPreviewScreen extends StatelessWidget {
   final File pdfFile;
@@ -11,16 +11,12 @@ class PdfPreviewScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("PDF Preview")),
-      body: PDFView(
-        filePath: pdfFile.path,
-        enableSwipe: true,
-        swipeHorizontal: false,
-        autoSpacing: true,
-        pageFling: true,
-        onError: (e) {
-          ScaffoldMessenger.of(
-            context,
-          ).showSnackBar(SnackBar(content: Text("Error loading PDF: $e")));
+      body: SfPdfViewer.file(
+        pdfFile,
+        onDocumentLoadFailed: (details) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Error loading PDF: ${details.description}")),
+          );
         },
       ),
     );
