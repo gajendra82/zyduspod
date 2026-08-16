fc84de4f1ed10fb18722f0f9885b8ce2cf419687
gajendra82 <gajendra.kumar@globalspace.in>
2026-08-16 23:54:40 +0530

    grn

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
-//const String API_BASE_URL = "https://zydus.mediola.in/pod_dev/api/";
-const String API_BASE_URL = "https://zydus.mediola.in/development/api/";
+const String API_BASE_URL = "https://zydus.mediola.in/pod_dev/api/";
+//const String API_BASE_URL = "https://zydus.mediola.in/development/api/";
 //const String API_BASE_URL = "http://192.168.1.42:8000/api/";
 //const String API_BASE_URL = "http://localhost:8000/api/";
 
